ccb54b8c313edb2bca40974a3cabae45a422e040
Frida Berg <fb223gw@student.lnu.se>
2026-02-03 22:39:55 +0100

    Skulle börja med Practice_write_run_test först

--- js/practice/practice_write_run_tests/tests/password.test.js
+++ js/practice/practice_write_run_tests/tests/password.test.js
@@ -1,8 +1,8 @@
 
 // Select one of the Password versions to test
 
-import { Password } from '../src/BugDoesNotHash'
-// import { Password } from '../src/BugDoesNotTrim'
+// import { Password } from '../src/BugDoesNotHash'
+import { Password } from '../src/BugDoesNotTrim'
 // import { Password } from '../src/BugisPasswordAlwaysSame'
 // import { Password } from '../src/BugMissingNumberCheck'
 // import { Password } from '../src/BugMissingPasswordCheck'
@@ -15,11 +15,27 @@ import { Password } from '../src/BugDoesNotHash'
 
 describe('Password class, test suite', () => {
     //put constants here to increase readability
-    const emptyPassword = '';
+    const passwordWithoutNumber = 'SecretPassword';
+    const pw = "ValidPassword123";
+    const passwordTooShortWithSpaces = "  1234567890  ";
 
-    test('replace this test with one of your own and add more', () => {
-        expect(true).toBe(true);
+    test('Constructor Should Throw error for password without numbers', () => {
+        expect(() => {
+            new Password(passwordWithoutNumber);
+        }).toThrow();
     });
 
-    //Add your tests here
+    test('isPasswordSame Should Return True For Identical Passwords', () => {
+
+        const p1 = new Password(pw);
+        const p2 = new Password(pw);
+
+        expect(p1.isPasswordSame(p2)).toBe(true);
+    });
+
+    test('constructor Should Throw Error For Short Passwords After Trimming', () => {
+        expect(() => {
+            new Password(passwordTooShortWithSpaces);
+        }).toThrow("Too short password");
+    });
 });
